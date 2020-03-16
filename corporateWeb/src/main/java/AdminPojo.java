ba6e8284ae7c38519e9e6e4aff4782302d14e774
Sakul Thakuri <sazzrulz38@gmail.com>
2020-03-16 18:10:15 +0530

    resolved merge conflict in adminregitser

--- corporateWeb/src/main/java/AdminPojo.java
+++ corporateWeb/src/main/java/AdminPojo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import javax.persistence.*; 
 
 
@@ -31,37 +30,3 @@ public class AdminPojo {
         return password;
     }
 }
-||||||| merged common ancestors
-=======
-import javax.persistence.*; 
-
-
-@Entity
-@Table(name="AdminInfo")
-public class AdminPojo {
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private String name;
-    private String email;
-    private String password;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setEmail(String email) {
-        this.email= email;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setPassword (String password) {
-        this.password = password;
-    }
-    public String getPassword(){
-        return password;
-    }
-}
->>>>>>> dfc94dfae6b38dc5146af9ef82cc5bcb5edf98d1
